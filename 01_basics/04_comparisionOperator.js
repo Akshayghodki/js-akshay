dd304b4e9515dca2013db0be87afb4d22734d93d
Akshay Ghodki <97594145+Akshayghodki@users.noreply.github.com>
2023-09-25 05:31:39 +0000

    conversion &comparision

---
+++ 01_basics/04_comparisionOperator.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);    output => true
+// console.log(2 >= 1);   output => true
+// console.log(2 < 1);    output => false
+// console.log(2 == 1);   output => false
+// console.log(2 != 1);   output => true
+
+
+// console.log("2" > 1); // output => true
+// console.log("02" > 1); // output => true
+
+console.log(null > 0);   // output => false
+console.log(null == 0);  // output => false
+console.log(null >= 0);  // output => true
+
+console.log(undefined == 0);  //output => FALSE
+console.log(undefined > 0);  //output => FALSE
+console.log(undefined < 0);  //output => FALSE
+
+// === 
+
+console.log("2" === 2); // OUTPUT false
